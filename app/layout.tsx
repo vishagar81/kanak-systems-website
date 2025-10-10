e9036383dc9c1019a0c546182f2511bd86f51173
VishalAgarwal <v_vishalagarwal@tfl.gov.uk>
2025-10-10 17:21:11 +0100

    Updated the Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'AI Partner for Your Business | kanaksystems.com',
+  title: 'AI Partner for Your Business | Kanak Systems',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
